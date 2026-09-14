aa85be251956ad8628abf36c73a3bf2db419c210
Santosh Kumar <santosh.sah@geekyants.com>
2026-09-15 00:13:51 +0530

    refactor: udpate footer and add into private layout

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,4 +1,5 @@
 import { auth } from '@/auth';
+import Footer from '@/components/layout/Footer';
 import { Navbar } from '@/components/layout/Navbar';
 import { redirect, notFound } from 'next/navigation';
 
@@ -26,6 +27,9 @@ export default async function PrivateLayout({ children, params }: Readonly<Priva
     <div className="flex min-h-screen flex-col bg-gray-100 antialiased">
       <Navbar user={session.user} />
       <main className="flex w-full flex-1 flex-col">{children}</main>
+      <div className="mx-auto mb-4 w-full max-w-7xl px-12">
+        <Footer />
+      </div>
     </div>
   );
 }
--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mx-auto flex w-full items-center justify-center rounded-lg bg-white py-6 shadow">
+      <p className="text-sm text-gray-500">© 2024 tentwenty. All rights reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
