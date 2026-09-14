50cdd3bc9818d0351d7a12acb9f8f4752cd12389
Santosh Kumar <santosh.sah@geekyants.com>
2026-09-14 15:46:22 +0530

    refactor: dashboardLayout to proviateLayout

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,13 +1,30 @@
+import { auth } from '@/auth';
 import { Navbar } from '@/components/layout/Navbar';
+import { redirect, notFound } from 'next/navigation';
 
-export default function PrivateLayout({
-  children,
-}: Readonly<{
+interface PrivateLayoutProps {
   children: React.ReactNode;
-}>) {
+  params: Promise<{ orgSlug?: string }>;
+}
+
+export default async function PrivateLayout({ children, params }: Readonly<PrivateLayoutProps>) {
+  const session = await auth();
+  if (!session?.user) {
+    redirect('/');
+  }
+
+  const { orgSlug } = await params;
+
+  if (orgSlug) {
+    const hasAccessToOrg = session.user.orgs?.some((org) => org.slug === orgSlug);
+    if (!hasAccessToOrg) {
+      notFound();
+    }
+  }
+
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Navbar />
+    <div className="flex min-h-screen flex-col bg-gray-100 antialiased">
+      <Navbar user={session.user} />
       <main className="flex w-full flex-1 flex-col">{children}</main>
     </div>
   );
